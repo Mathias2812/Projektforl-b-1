75b7e8fd9d76727f3c17f32143755bc724b98522
gouled83 <gouled13@gmail.com>
2024-05-21 12:18:34 +0200

    done

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 // Set the dimensions and margins of the graph
-const margin = {top: 20, right: 180, bottom: 30, left: 50},
+const margin = {top: 20, right: 180, bottom: 50, left: 50},
 width = 800 - margin.left - margin.right,
-height = 400 - margin.top - margin.bottom;
+height = 500 - margin.top - margin.bottom;
 
 // Append the svg object to the body of the page
 const svg = d3.select("#chart")
@@ -35,10 +35,11 @@ d3.csv("share-plastic-waste-recycled.csv").then(data => {
   // Define the line
   const line = d3.line()
     .x(d => x(d.Year))
-    .y(d => y(d.Value));
+    .y(d => y(d.Value))
+    .curve(d3.curveMonotoneX);  // Add smooth curve
 
-  // Set up color scale
-  const color = d3.scaleOrdinal(d3.schemeCategory10)
+  // Set up color scale with a better palette
+  const color = d3.scaleOrdinal(d3.schemeTableau10)
     .domain(countries);
 
   // Group data by country
@@ -53,18 +54,43 @@ d3.csv("share-plastic-waste-recycled.csv").then(data => {
     .attr("d", d => line(d[1]))
     .style("fill", "none")
     .style("stroke", d => color(d[0]))
-    .style("stroke-width", "2px");
+    .style("stroke-width", "2.5px")
+    .style("opacity", 0.7)
+    .on("mouseover", function(event, d) {
+      d3.selectAll(".line").style("opacity", 0.1);
+      d3.select(this).style("opacity", 1).style("stroke-width", "4px");
+    })
+    .on("mouseout", function(event, d) {
+      d3.selectAll(".line").style("opacity", 0.7).style("stroke-width", "2.5px");
+    });
 
   // Add the X Axis
   svg.append("g")
     .attr("class", "x-axis")
     .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x).ticks(10).tickFormat(d3.timeFormat("%Y")))
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", 40)
+    .attr("fill", "#000")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "14px")
+    .attr("font-weight", "bold")
+    .text("Year");
 
   // Add the Y Axis
   svg.append("g")
     .attr("class", "y-axis")
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y).ticks(10).tickFormat(d => `${d}%`))
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -40)
+    .attr("x", -height / 2)
+    .attr("fill", "#000")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "14px")
+    .attr("font-weight", "bold")
+    .text("Percentage");
 
   // Add legend
   const legend = svg.selectAll(".legend")
@@ -83,5 +109,6 @@ d3.csv("share-plastic-waste-recycled.csv").then(data => {
     .attr("x", 24)
     .attr("y", 9)
     .attr("dy", ".35em")
+    .style("font-size", "12px")
     .text(d => d);
 });
