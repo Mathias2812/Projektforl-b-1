841c9102ecbadb50aa2000f5a0c679e0dc6afa61
gouled83 <gouled13@gmail.com>
2024-05-21 12:40:49 +0200

    d

--- continent.js
+++ continent.js
@@ -1,6 +1,6 @@
 // Set the dimensions and margins of the graph
 const margin = {top: 20, right: 180, bottom: 50, left: 50},
-width = 800 - margin.left - margin.right,
+width = 1500 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
 // Append the svg object to the body of the page
