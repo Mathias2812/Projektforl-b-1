7c5d6dd47833c193a6cf4429a51c385d391fc659
gouled83 <gouled13@gmail.com>
2024-05-21 12:13:50 +0200

    der w's in the chat!

--- queries.js
+++ queries.js
@@ -5,88 +5,83 @@ height = 400 - margin.top - margin.bottom;
 
 // Append the svg object to the body of the page
 const svg = d3.select("#chart")
-.append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", `translate(${margin.left},${margin.top})`);
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // Parse the Year / time
 const parseTime = d3.timeParse("%Y");
 
 // Load the CSV file
 d3.csv("share-plastic-waste-recycled.csv").then(data => {
-data.forEach(d => {
-d.Year = parseTime(d.Year);
-d.Value = +d.Value;
-});
+  data.forEach(d => {
+    d.Year = parseTime(d.Year);
+    d.Value = +d.Value;
+  });
 
-// Extract unique countries for the dropdown
-const countries = Array.from(new Set(data.map(d => d.Country)));
+  // Extract unique countries for the color scale
+  const countries = Array.from(new Set(data.map(d => d.Country)));
 
-// Populate the country dropdown
-const countrySelect = d3.select("#countrySelect");
-countrySelect.selectAll("option")
-.data(countries)
-.enter()
-.append("option")
-.text(d => d);
+  // Set the ranges
+  const x = d3.scaleTime().range([0, width]);
+  const y = d3.scaleLinear().range([height, 0]);
 
-// Function to update the chart
-function updateChart(country) {
-// Filter data for the selected country
-const filteredData = data.filter(d => d.Country === country);
+  // Scale the range of the data
+  x.domain(d3.extent(data, d => d.Year));
+  y.domain([0, d3.max(data, d => d.Value)]);
 
-// Check if filtered data is empty
-if (filteredData.length === 0) {
-console.log("No data for the selected country");
-return;
-}
+  // Define the line
+  const line = d3.line()
+    .x(d => x(d.Year))
+    .y(d => y(d.Value));
 
-// Set the ranges
-const x = d3.scaleTime().range([0, width]);
-const y = d3.scaleLinear().range([height, 0]);
+  // Set up color scale
+  const color = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(countries);
 
-// Define the area
-const area = d3.area()
-.x(d => x(d.Year))
-.y0(height)
-.y1(d => y(d.Value));
+  // Group data by country
+  const dataByCountry = d3.groups(data, d => d.Country);
 
-// Scale the range of the data
-x.domain(d3.extent(filteredData, d => d.Year));
-y.domain([0, d3.max(filteredData, d => d.Value)]);
+  // Add the lines for each country
+  svg.selectAll(".line")
+    .data(dataByCountry)
+    .enter()
+    .append("path")
+    .attr("class", "line")
+    .attr("d", d => line(d[1]))
+    .style("fill", "none")
+    .style("stroke", d => color(d[0]))
+    .style("stroke-width", "2px");
 
-// Remove any existing path
-svg.selectAll(".area").remove();
+  // Add the X Axis
+  svg.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x));
 
-// Add the area path
-svg.append("path")
-.data([filteredData])
-.attr("class", "area")
-.attr("d", area)
-.style("fill", "steelblue");
+  // Add the Y Axis
+  svg.append("g")
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(y));
 
-// Add the X Axis
-svg.selectAll(".x-axis").remove();
-svg.append("g")
-.attr("class", "x-axis")
-.attr("transform", `translate(0,${height})`)
-.call(d3.axisBottom(x));
+  // Add legend
+  const legend = svg.selectAll(".legend")
+    .data(countries)
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => `translate(${width + 20},${i * 20})`);
 
-// Add the Y Axis
-svg.selectAll(".y-axis").remove();
-svg.append("g")
-.attr("class", "y-axis")
-.call(d3.axisLeft(y));
-}
+  legend.append("rect")
+    .attr("x", 0)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", color);
 
-// Event listener for country dropdown
-countrySelect.on("change", () => {
-const selectedCountry = countrySelect.node().value;
-updateChart(selectedCountry);
-});
-
-// Initial chart update
-updateChart(countries[0]);
+  legend.append("text")
+    .attr("x", 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .text(d => d);
 });
