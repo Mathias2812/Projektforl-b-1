6655fc5e1696cd64e77405e442b71f975cfaf8f1
gouled83 <gouled13@gmail.com>
2024-05-21 09:50:04 +0200

    script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 100, bottom: 30, left: 30},
-    width = 460 - margin.left - margin.right,
+var margin = {top: 10, right: 100, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -15,75 +15,69 @@ var svg = d3.select("#my_dataviz")
 // Read the data
 d3.csv("share-plastic-waste-recycled.csv", function(data) {
 
-    // List of groups (here I have one group per column)
-    var allGroup = ["valueA", "valueB", "valueC"]
+    // List of entities
+    var entities = [...new Set(data.map(function(d) { return d.entity; }))];
 
-    // add the options to the button
-    d3.select("#selectButton")
-      .selectAll('myOptions')
-      .data(allGroup)
-      .enter()
-      .append('option')
-      .text(function (d) { return d; }) // text showed in the menu
-      .attr("value", function (d) { return d; }) // corresponding value returned by the button
+    // List of years
+    var years = data.map(function(d) { return d.year; });
 
-    // A color scale: one color for each group
+    // List of groups (here I have one group per entity)
+    var allGroup = entities;
+
+    // A color scale: one color for each entity
     var myColor = d3.scaleOrdinal()
       .domain(allGroup)
       .range(d3.schemeSet2);
 
-    // Add X axis --> it is a date format
-    var x = d3.scaleLinear()
-      .domain([0,10])
-      .range([ 0, width ]);
+    // Add X axis
+    var x = d3.scaleBand()
+      .domain(years)
+      .range([0, width])
+      .padding(0.2);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain( [0,20])
-      .range([ height, 0 ]);
+      .domain([0, d3.max(data, function(d) { return +d['share of waste recycled']; })])
+      .range([height, 0]);
     svg.append("g")
       .call(d3.axisLeft(y));
 
-    // Initialize line with group a
-    var line = svg
-      .append('g')
-      .append("path")
-        .datum(data)
-        .attr("d", d3.line()
-          .x(function(d) { return x(+d.time) })
-          .y(function(d) { return y(+d.valueA) })
-        )
-        .attr("stroke", function(d){ return myColor("valueA") })
-        .style("stroke-width", 4)
-        .style("fill", "none")
+    // Add lines
+    var line = svg.selectAll(".line")
+      .data(allGroup)
+      .enter()
+      .append("g")
+      .attr("class", "line");
 
-    // A function that update the chart
-    function update(selectedGroup) {
+    line.append("path")
+      .datum(function(d) {
+        return data.filter(function(datum) {
+          return datum.entity === d;
+        });
+      })
+      .attr("fill", "none")
+      .attr("stroke", function(d) { return myColor(d[0].entity); })
+      .attr("stroke-width", 2.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.year) + x.bandwidth() / 2; })
+        .y(function(d) { return y(d['share of waste recycled']); })
+      );
 
-      // Create new data with the selection?
-      var dataFilter = data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
+    // Add dots
+    line.selectAll("dot")
+      .data(function(d) {
+        return data.filter(function(datum) {
+          return datum.entity === d;
+        });
+      })
+      .enter()
+      .append("circle")
+      .attr("fill", function(d) { return myColor(d.entity); })
+      .attr("cx", function(d) { return x(d.year) + x.bandwidth() / 2; })
+      .attr("cy", function(d) { return y(d['share of waste recycled']); })
+      .attr("r", 5);
 
-      // Give these new data to update line
-      line
-          .datum(dataFilter)
-          .transition()
-          .duration(1000)
-          .attr("d", d3.line()
-            .x(function(d) { return x(+d.time) })
-            .y(function(d) { return y(+d.value) })
-          )
-          .attr("stroke", function(d){ return myColor(selectedGroup) })
-    }
-
-    // When the button is changed, run the updateChart function
-    d3.select("#selectButton").on("change", function(d) {
-        // recover the option that has been chosen
-        var selectedOption = d3.select(this).property("value")
-        // run the updateChart function with this selected option
-        update(selectedOption)
-    })
-
-})
+});
