966e9396dd90065c8ab152885b4b0efaf1a5e980
Mathias2812 <mathias.sparre2812@gmail.com>
2024-05-22 08:53:42 +0200

    virker nu

--- scripts.js
+++ scripts.js
@@ -1,74 +1,74 @@
+const apiUrl = 'http://localhost:4000/Data';
+let worldData = [];
 
-// Ændr størrelsen på SVG-elementet
-let svgWidth = 1200; // Ny bredde
-let svgHeight = 600; // Ny højde
-let svg = d3.select("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+// Fetch data from the API
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        worldData = data;
+        console.log('World Data:', worldData);
 
-// Opdater størrelsen på kortets projection
-let projection = d3.geoMercator()
-  .scale(120) // Juster skalaen efter behov
-  .center([0, 20])
-  .translate([svgWidth / 2, svgHeight / 2]);
+        // Load external GeoJSON data and draw the map
+        loadGeoJsonAndDrawMap();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
-// Data and color scale
-let data = d3.map();
-let colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-  .range(d3.schemeBlues[7]);
+function loadGeoJsonAndDrawMap() {
+    d3.queue()
+        .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+        .await(function(error, geoJsonData) {
+            if (error) throw error;
+            drawMap(geoJsonData, worldData);
+        });
+}
 
-// Load external data and boot
-d3.queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-  .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
-  .await(ready);
+function drawMap(geoJsonData, worldData) {
+    let svgWidth = 1200;
+    let svgHeight = 600;
+    let svg = d3.select("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
 
-function ready(error, topo) {
+    let projection = d3.geoMercator()
+        .scale(120)
+        .center([0, 20])
+        .translate([svgWidth / 2, svgHeight / 2]);
 
-  // When the mouse is on the map
-  let mouseOver = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .5)
-      .style("stroke", "black")
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
+    let colorScale = d3.scaleLinear()
+        .domain(d3.extent(worldData, d => d.mwi_value))
+        .range(["blue", "red"]);
 
-  }
+    let dataMap = new Map(worldData.map(d => [d.country_code, d]));
 
-  // When the mouse is off the map
-  let mouseLeave = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .8)
-    d3.select(this)
-      .transition()
-      .duration(200)
-  }
-
-  // Draw the map
-  svg.append("g")
-    .selectAll("path")
-    .data(topo.features)
-    .enter()
-    .append("path")
-      // draw each country
-      .attr("d", d3.geoPath()
-        .projection(projection)
-      )
-      // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(d.id) || 0;
-        return colorScale(d.total);
-      })
-      .style("stroke", "black")
-      .attr("class", function(d){ return "Country" } )
-      .style("opacity", .8)
-      .on("mouseover", mouseOver )
-      .on("mouseleave", mouseLeave )
-    }
+    svg.append("g")
+        .selectAll("path")
+        .data(geoJsonData.features)
+        .enter()
+        .append("path")
+        .attr("d", d3.geoPath().projection(projection))
+        .attr("fill", d => {
+            let countryData = dataMap.get(d.id);
+            return countryData ? colorScale(countryData.mwi_value) : "#ccc";
+        })
+        .style("stroke", "black")
+        .attr("class", "Country")
+        .style("opacity", .8)
+        .on("mouseover", function(event, d) {
+            d3.select(this).style("opacity", 1);
+            let countryData = dataMap.get(d.id);
+            if (countryData) {
+                console.log(`Country: ${countryData.country_name}, MWI Value: ${countryData.mwi_value}, Total Waste: ${countryData.total_waste}`);
+                // Display a tooltip or other UI element with country data here
+            }
+        })
+        .on("mouseleave", function() {
+            d3.select(this).style("opacity", .8);
+        });
+}
