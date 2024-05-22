e8853fc195c31f4837c28b756d764c9cd150fa2a
Mathias2812 <mathias.sparre2812@gmail.com>
2024-05-22 20:25:16 +0200

    ændring

--- quiz.js
+++ quiz.js
@@ -1,12 +1,12 @@
 // Spørgsmål array
 const questions = [
     { question: "Only 9% of all plastic produced is recycled?.", answer: true, explanation:"The reason being is due to the extensive costs it takes to produce recycled plastic." },
-    { question: "2 million plastic bags are used every minute worldwide?.", answer: false,explanation: "It's 'only' 1 million plastic bags that are being used every minute" },
+    { question: "500.000 plastic bags are used every minute worldwide?.", answer: false,explanation: "It's actually 1 million plastic bags that are being used every minute" },
     { question: "10 million marine animals die each year from plastic waste alone?.", answer: false,explanation: "It's actually approximately 100 million marine animals that die each year from plastic waste alone." },
-    { question: "5 million plastic bottles are bought every minute?.", answer: false, explanation:"It's actually 'only' 1 million plastic bottles that are bought every minute." },
+    { question: "670.000 plastic bottles are bought every minute?.", answer: false, explanation:"It's actually 1 million plastic bottles that are bought every minute." },
     { question: "Nearly one-quarter of the world's plastic waste is mismanaged or littered?.", answer: true, explanation: "That is around 82 million tonnes mismanaged,and a quarter of that is leaked to the environment." },
     { question: "Plastic bottles slowly dissipates and eventually disappears?", answer: false, explanation: "A plastic bottle can last for 450 years in the marine environment, slowly fragmenting into smaller and smaller pieces which never truly disappear." },
-    { question: "There will be more plastic in our oceans than fish by 2080?.", answer: false, explanation:"There will actually be more plastic in the oceans by 2050 than fish, if we don't do anything about it." },
+    { question: "There will be more plastic in our oceans than fish by 2100?.", answer: false, explanation:"There will actually be more plastic in the oceans by 2050 than fish, if we don't do anything about it." },
     { question: "Plastic is made primarily from fossil fuel.", answer: true, explanation:"Around 90% of all plastic is made of fossil fuel, and it can be very toxic to you and the world if managed wrong." },
     { question: "It takes 7 gallons of water to make one pound of plastic?.", answer: false, explanation:" It takes approximately 22 gallons of water to make 1 pound of plastic" },
     { question:"If everyone switched to reusable bottles and carton water, we would be able to save enough energy to power New York City for an entire month?", answer: true, explanation:"Now imagine if we started using recycled plastic or reusable things, how much energy we would save. Plus spare the environment"},
