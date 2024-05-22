de7edbead9d1a4085170102aa35359739c85ae79
Frede52 <frederikkarno@gmail.com>
2024-05-22 11:25:43 +0200

    new changes

--- barchart.js
+++ barchart.js
@@ -32,55 +32,134 @@ Promise.all(apiUrls.map(url => fetch(url).then(response => {
 
 
 
- // set the dimensions and margins of the graph
- let margin = {top: 30, right: 30, bottom: 70, left: 60},
-     width = 460 - margin.left - margin.right,
-     height = 400 - margin.top - margin.bottom;
- 
- // append the svg object to the body of the page
- let svg = d3.select("#top5")
-   .append("svg")
-     .attr("width", width + margin.left + margin.right)
-     .attr("height", height + margin.top + margin.bottom)
-   .append("g")
-     .attr("transform",
-           "translate(" + margin.left + "," + margin.top + ")");
- 
- // X axis
- let x = d3.scaleBand()
-   .range([ 0, width ])
-   .domain(mwiOutliers.map(function(d) { return d.group; }))
-   .padding(0.2);
- svg.append("g")
-   .attr("transform", "translate(0," + height + ")")
-   .call(d3.axisBottom(x))
- 
- // Add Y axis
- let y = d3.scaleLinear()
-   .domain([0, 20])
-   .range([ height, 0]);
- svg.append("g")
-   .attr("class", "myYaxis")
-   .call(d3.axisLeft(y));
- 
- // A function that create / update the plot for a given variable:
- function update(data) {
- 
-   let u = svg.selectAll("rect")
-     .data(data)
- 
-   u
-     .enter()
-     .append("rect")
-     .merge(u)
-     .transition()
-     .duration(1000)
-       .attr("x", function(d) { return x(d.group); })
-       .attr("y", function(d) { return y(d.value); })
-       .attr("width", x.bandwidth())
-       .attr("height", function(d) { return height - y(d.value); })
-       .attr("fill", "#69b3a2")
- }
- 
- // Initialize the plot with the first dataset
- update(mwiOutliers)
+// set the dimensions and margins of the graph
+let margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 560 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+let svg = d3.select("#top5")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// X axis
+let x = d3.scaleBand()
+  .range([ 0, width ])
+  .padding(0.2);
+
+// Add Y axis
+let y = d3.scaleLinear()
+  .range([ height, 0]);
+
+// A function that create / update the plot for a given variable:
+function update(data) {
+  // Update the domain of the x scale
+  x.domain(data.map(function(d) { return d.country_name; }));
+
+  // Update the domain of the y scale based on the maximum value in the dataset
+  y.domain([0, d3.max(data, function(d) { return d.mwi_value; })]);
+
+  // Draw the X axis
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+    attr("fill", "white");
+
+  // Draw the Y axis
+  svg.append("g")
+    .attr("class", "myYaxis")
+    .call(d3.axisLeft(y));
+    attr("fill", "white");
+
+  // Select all bars and bind data
+  let bars = svg.selectAll("rect")
+    .data(data);
+
+  // Enter new bars
+  bars.enter()
+    .append("rect")
+    .attr("x", function(d) { return x(d.country_name); })
+    .attr("width", x.bandwidth())
+    .merge(bars)
+    .transition()
+    .duration(1000)
+      .attr("y", function(d) { return y(d.mwi_value); })
+      .attr("height", function(d) { return height - y(d.mwi_value); })
+      .attr("fill", "#33AFFF");
+}
+
+
+// Function to update the bar chart
+function update(data) {
+  // Update the domain of the x scale
+  x.domain(data.map(function(d) { return d.country_name; }));
+
+  // Update the domain of the y scale based on the maximum value in the dataset
+  y.domain([0, d3.max(data, function(d) { return d.total_waste || d.mwi_value; })]);
+
+ // Redraw the X axis
+svg.select(".myXaxis")
+.call(d3.axisBottom(x))
+.selectAll("text")
+.attr("transform", "translate(-10,0)rotate(-45)")
+.attr("fill", "white")
+.style("text-anchor", "end");
+
+// Redraw the Y axis
+svg.select(".myYaxis")
+.call(d3.axisLeft(y))
+.selectAll("text")
+.attr("fill", "white");
+
+  // Remove the existing bars and labels
+  svg.selectAll("rect").remove();
+  svg.selectAll(".bar-label").remove();
+
+  // Draw the bars
+  svg.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", function(d) { return x(d.country_name); })
+    .attr("width", x.bandwidth())
+    .attr("y", function(d) { return y(d.total_waste || d.mwi_value); })
+    .attr("height", function(d) { return height - y(d.total_waste || d.mwi_value); })
+    .attr("fill", "#33AFFF");
+
+  // Add labels with data value
+  svg.selectAll("rect")
+    .each(function(d) {
+      svg.append("text")
+        .attr("class", "bar-label")
+        .attr("x", x(d.country_name) + x.bandwidth() / 2)
+        .attr("y", y(d.total_waste || d.mwi_value) - 5) // Adjust the y position for better positioning
+        .attr("text-anchor", "middle")
+        .attr("fill", "white")
+        .text(d.total_waste || d.mwi_value);
+    });
+}
+
+// Initialize the X axis
+svg.append("g")
+  .attr("class", "myXaxis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-45)")
+  .attr("fill", "white")
+  .style("text-anchor", "end");
+
+// Initialize the Y axis
+svg.append("g")
+  .attr("class", "myYaxis")
+  .call(d3.axisLeft(y));
+
+// Event listener for the button click
+document.getElementById("updateButton").addEventListener("click", function() {
+  // Call the update function with the wasteOutliers data
+  update(wasteOutliers);
+});
