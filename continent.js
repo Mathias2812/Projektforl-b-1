eb7515ee7cb399be4e4e09972a653f6fc07d0f13
Mathias2812 <mathias.sparre2812@gmail.com>
2024-05-24 10:57:11 +0200

    ee

--- continent.js
+++ continent.js
@@ -3,6 +3,9 @@ const margin = {top: 20, right: 180, bottom: 50, left: 50},
 width = 1200 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
+// Clear any existing SVG
+d3.select("#chart").selectAll("*").remove();
+
 // Append the svg object to the body of the page
 const svg = d3.select("#chart")
   .append("svg")
@@ -69,43 +72,51 @@ d3.csv("share-plastic-waste-recycled.csv").then(data => {
     .attr("class", "x-axis")
     .attr("transform", `translate(0,${height})`)
     .call(d3.axisBottom(x).ticks(10).tickFormat(d3.timeFormat("%Y")))
-    .append("text")
-    .attr("x", width / 2)
-    .attr("y", 40)
-    .attr("fill", "white")
-    .attr("text-anchor", "middle")
-    .attr("font-size", "14px")
-    .attr("font-weight", "bold")
-    .text("Year");
+    .selectAll("text")
+    .style("fill", "white");  // Set text color to white
 
   // Add the Y Axis
   svg.append("g")
     .attr("class", "y-axis")
     .call(d3.axisLeft(y).ticks(10).tickFormat(d => `${d}%`))
-    .append("text")
+    .selectAll("text")
+    .style("fill", "white");  // Set text color to white
+
+  // Add X Axis label
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height + 40)
+    .style("fill", "white")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "17px")
+    .attr("font-weight", "bold")
+    .text("Year");
+
+  // Add Y Axis label
+  svg.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", -40)
     .attr("x", -height / 2)
-    .attr("fill", "white")
+    .style("fill", "white")
     .attr("text-anchor", "middle")
-    .attr("font-size", "14px")
+    .attr("font-size", "17px")
     .attr("font-weight", "bold")
-    .text("Percentage");
+    .text("Percentage of plastic recycled");
 
   // Add legend
-  const legend = svg.selectAll(".legend")
+  const visual = svg.selectAll(".visual")
     .data(countries)
     .enter().append("g")
     .attr("class", "legend")
     .attr("transform", (d, i) => `translate(${width + 20},${i * 20})`);
 
-  legend.append("rect")
+  visual.append("rect")
     .attr("x", 0)
     .attr("width", 18)
     .attr("height", 18)
     .style("fill", color);
 
-  legend.append("text")
+  visual.append("text")
     .attr("x", 24)
     .attr("y", 9)
     .attr("dy", ".35em")
