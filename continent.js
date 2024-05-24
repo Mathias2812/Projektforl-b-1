7681bdd2a04f46c58574177db86b4e5ce91096ce
Mathias2812 <mathias.sparre2812@gmail.com>
2024-05-24 09:13:25 +0200

    ee

--- continent.js
+++ continent.js
@@ -1,6 +1,6 @@
 // Set the dimensions and margins of the graph
 const margin = {top: 20, right: 180, bottom: 50, left: 50},
-width = 1500 - margin.left - margin.right,
+width = 1200 - margin.left - margin.right,
 height = 500 - margin.top - margin.bottom;
 
 // Append the svg object to the body of the page
@@ -72,7 +72,7 @@ d3.csv("share-plastic-waste-recycled.csv").then(data => {
     .append("text")
     .attr("x", width / 2)
     .attr("y", 40)
-    .attr("fill", "#000")
+    .attr("fill", "white")
     .attr("text-anchor", "middle")
     .attr("font-size", "14px")
     .attr("font-weight", "bold")
@@ -86,7 +86,7 @@ d3.csv("share-plastic-waste-recycled.csv").then(data => {
     .attr("transform", "rotate(-90)")
     .attr("y", -40)
     .attr("x", -height / 2)
-    .attr("fill", "#000")
+    .attr("fill", "white")
     .attr("text-anchor", "middle")
     .attr("font-size", "14px")
     .attr("font-weight", "bold")
