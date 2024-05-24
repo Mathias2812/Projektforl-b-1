cb81b9f7a992148ee0139957e46091157cb35e67
Mathias2812 <mathias.sparre2812@gmail.com>
2024-05-24 09:26:51 +0200

    ee

--- scripts.js
+++ scripts.js
@@ -1,74 +1,126 @@
+// Set the dimensions and margins of the graph
+const margin = {top: 20, right: 180, bottom: 50, left: 50},
+width = 1200 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
 
-// Ændr størrelsen på SVG-elementet
-let svgWidth = 1200; // Ny bredde
-let svgHeight = 600; // Ny højde
-let svg = d3.select("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+// Clear any existing SVG
+d3.select("#chart").selectAll("*").remove();
 
-// Opdater størrelsen på kortets projection
-let projection = d3.geoMercator()
-  .scale(120) // Juster skalaen efter behov
-  .center([0, 20])
-  .translate([svgWidth / 2, svgHeight / 2]);
+// Append the svg object to the body of the page
+const svg = d3.select("#chart")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Data and color scale
-let data = d3.map();
-let colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-  .range(d3.schemeBlues[7]);
+// Parse the Year / time
+const parseTime = d3.timeParse("%Y");
 
-// Load external data and boot
-d3.queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-  .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); })
-  .await(ready);
+// Load the CSV file
+d3.csv("share-plastic-waste-recycled.csv").then(data => {
+  data.forEach(d => {
+    d.Year = parseTime(d.Year);
+    d.Value = +d.Value;
+  });
 
-function ready(error, topo) {
+  // Extract unique countries for the color scale
+  const countries = Array.from(new Set(data.map(d => d.Country)));
 
-  // When the mouse is on the map
-  let mouseOver = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .5)
-      .style("stroke", "black")
-    d3.select(this)
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
+  // Set the ranges
+  const x = d3.scaleTime().range([0, width]);
+  const y = d3.scaleLinear().range([height, 0]);
 
-  }
+  // Scale the range of the data
+  x.domain(d3.extent(data, d => d.Year));
+  y.domain([0, d3.max(data, d => d.Value)]);
 
-  // When the mouse is off the map
-  let mouseLeave = function(d) {
-    d3.selectAll(".Country")
-      .transition()
-      .duration(200)
-      .style("opacity", .8)
-    d3.select(this)
-      .transition()
-      .duration(200)
-  }
+  // Define the line
+  const line = d3.line()
+    .x(d => x(d.Year))
+    .y(d => y(d.Value))
+    .curve(d3.curveMonotoneX);  // Add smooth curve
 
-  // Draw the map
-  svg.append("g")
-    .selectAll("path")
-    .data(topo.features)
+  // Set up color scale with a better palette
+  const color = d3.scaleOrdinal(d3.schemeTableau10)
+    .domain(countries);
+
+  // Group data by country
+  const dataByCountry = d3.groups(data, d => d.Country);
+
+  // Add the lines for each country
+  svg.selectAll(".line")
+    .data(dataByCountry)
     .enter()
     .append("path")
-      // draw each country
-      .attr("d", d3.geoPath()
-        .projection(projection)
-      )
-      // set the color of each country
-      .attr("fill", function (d) {
-        d.total = data.get(d.id) || 0;
-        return colorScale(d.total);
-      })
-      .style("stroke", "black")
-      .attr("class", function(d){ return "Country" } )
-      .style("opacity", .8)
-      .on("mouseover", mouseOver )
-      .on("mouseleave", mouseLeave )
-    }
+    .attr("class", "line")
+    .attr("d", d => line(d[1]))
+    .style("fill", "none")
+    .style("stroke", d => color(d[0]))
+    .style("stroke-width", "2.5px")
+    .style("opacity", 0.7)
+    .on("mouseover", function(event, d) {
+      d3.selectAll(".line").style("opacity", 0.1);
+      d3.select(this).style("opacity", 1).style("stroke-width", "4px");
+    })
+    .on("mouseout", function(event, d) {
+      d3.selectAll(".line").style("opacity", 0.7).style("stroke-width", "2.5px");
+    });
+
+  // Add the X Axis
+  svg.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x).ticks(10).tickFormat(d3.timeFormat("%Y")))
+    .selectAll("text")
+    .style("fill", "white");  // Set text color to white
+
+  // Add the Y Axis
+  svg.append("g")
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(y).ticks(10).tickFormat(d => `${d}%`))
+    .selectAll("text")
+    .style("fill", "white");  // Set text color to white
+
+  // Add X Axis label
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height + 40)
+    .style("fill", "white")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "17px")
+    .attr("font-weight", "bold")
+    .text("Year");
+
+  // Add Y Axis label
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -40)
+    .attr("x", -height / 2)
+    .style("fill", "white")
+    .attr("text-anchor", "middle")
+    .attr("font-size", "17px")
+    .attr("font-weight", "bold")
+    .text("Percentage of plastic recycled");
+
+  // Add legend
+  const legend = svg.selectAll(".legend")
+    .data(countries)
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => `translate(${width + 20},${i * 20})`);
+
+  legend.append("rect")
+    .attr("x", 0)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", color);
+
+  legend.append("text")
+    .attr("x", 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("font-size", "12px")
+    .style("fill", "white")  // Set text color to white
+    .text(d => d);
+});
