d3fcc40d118c83e38fbad08b424bd38328de6947
Mathias2812 <mathias.sparre2812@gmail.com>
2024-05-25 12:18:56 +0200

    fixes

--- continentgraph.js
+++ continentgraph.js
@@ -1,5 +1,5 @@
  // Set the dimensions and margins of the graph
- const margin = {top: 20, right: 180, bottom: 50, left: 50},
+ const margin = {top: 20, right: 250, bottom: 100, left: 50},
  width = 1200 - margin.left - margin.right,
  height = 500 - margin.top - margin.bottom;
 
