0d3a522b5d3354dce546cdbbee5c788565b164d2
Frede52 <frederikkarno@gmail.com>
2024-05-28 11:57:53 +0200

    barplot works now

--- stackedbarplot.js
+++ stackedbarplot.js
@@ -1,7 +1,7 @@
 (function() {
 // set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 20, left: 50},
-      width = 650 - margin.left - margin.right,
+      width = 560 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
@@ -16,28 +16,41 @@ let svg = d3.select("#barplot")
 d3.csv("share-plastic-fate.csv", function(data) {
 
   // List of subgroups = header of the csv files = soil condition here
-  let subgroups = data.columns.slice(2);
+  let subgroups = data.columns.slice(1);
   console.log(subgroups)
 
   // List of groups = different ways of managing plastic waste
-  let groups = data.map(d => d.Entity);
+  let groups = d3.map(data, function(d){return(d.Entity)}).keys()
   console.log(groups)
 
   // Add X axis
   let x = d3.scaleBand()
       .domain(groups)
       .range([0, width])
-      .padding([0.2]);
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickSizeOuter(0));
+      .padding([0.3]);
 
+      svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0))
+    .selectAll("text")
+    .style("text-overflow", "ellipsis")
+    .style("overflow", "hidden")
+    .style("white-space", "nowrap")
+    .style("fill","white")
+    .text(function(d) {
+        return d.length > 10 ? d.substring(0, 10) + "..." : d;
+      });
+
+
+      
   // Add Y axis
   let y = d3.scaleLinear()
     .domain([0, 120])
     .range([ height, 0 ]);
   svg.append("g")
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y))
+    .selectAll("text")
+    .style("fill","white");
 
   // color palette = one color per subgroup
   let color = d3.scaleOrdinal()
@@ -46,41 +59,9 @@ d3.csv("share-plastic-fate.csv", function(data) {
 
   // Stack the data per subgroup
   let stackedData = d3.stack()
-    .keys(subgroups)
-    (data)
-
-
-
-
-    
-    // Create color legend
-let legend = d3.select("#barLegend")
-.append(svg)
-.attr("class", "legend")
-.attr("transform", "translate(" + (width - 150) + "," + (margin.top + 10) + ")");
-
-// Generate legend entries
-let legendEntries = legend.selectAll(".legend-entry")
-.data(subgroups)
-.enter().append("g")
-.attr("class", "legend-entry")
-.attr("transform", function(d, i) { return "translate(0," + (i * 20) + ")"; });
-
-// Add colored boxes to legend
-legendEntries.append("rect")
-.attr("x", 0)
-.attr("y", 0)
-.attr("width", 10)
-.attr("height", 10)
-.attr("fill", function(d) { return color(d); });
-
-// Add subgroup labels to legend
-legendEntries.append("text")
-.attr("x", 15)
-.attr("y", 8)
-.style("font-size", "12px")
-.text(function(d) { return d; });
-
+  .keys(subgroups)
+  (data)
+    console.log(stackedData)
 
 
 // Hover function for each subgroup!
@@ -125,25 +106,24 @@ let mouseleave = function(d) {
 }
 
 // Show the bars
-
 svg.append("g")
-    .selectAll("g")
-    // Enter in the stack data = loop key per key = group per group
-    .data(stackedData)
-    .enter().append("g")
-    .attr("fill", function(d) { return color(d.key); })
-    .attr("class", function(d){ return "myRect " + d.key }) // Add a class to each subgroup: their name
-    .selectAll("rect")
-    // enter a second time = loop subgroup per subgroup to add all rectangles
-    .data(function(d) { return d; })
-    .enter().append("rect")
+.selectAll("g")
+// Enter in the stack data = loop key per key = group per group
+.data(stackedData)
+.enter().append("g")
+  .attr("fill", function(d) { return color(d.key); })
+  .attr("class", function(d){ return "myRect " + d.key }) // Add a class to each subgroup: their name
+  .selectAll("rect")
+  // enter a second time = loop subgroup per subgroup to add all rectangles
+  .data(function(d) { return d; })
+  .enter().append("rect")
     .attr("x", function(d) { return x(d.data.Entity); })
     .attr("y", function(d) { return y(d[1]); })
     .attr("height", function(d) { return y(d[0]) - y(d[1]); })
     .attr("width",x.bandwidth())
     .attr("stroke", "grey")
-    .on("mouseover", mouseover)
-    .on("mouseleave", mouseleave);
+  .on("mouseover", mouseover)
+  .on("mouseleave", mouseleave)
 
 })
 })();
